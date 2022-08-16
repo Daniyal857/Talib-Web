81f3b0f0de2c291bfddd1e3777c0ffd499bed503
DaniyalKhan <daniyalkhan857@gmail.com>
2022-08-16 20:11:21 +0500

    about section UI

--- index.php
+++ index.php
@@ -4,7 +4,27 @@ require(__DIR__ . '/views/includes/header.php');
 ?>
 
 
-<!--MAIN BANNER Section 
+<!--ABOUT US Section 
   ================================================== -->
+<section class="about-us py-5">
+  <div class="container">
+    <div class="row">
+      <div class="col-6">
+        <div class="title mb-3 mt-3">About us</div>
+        <div class="desc  mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Dolor dis elit volutpat imperdiet facilisis. Venenatis sed donec iaculis tristique molestie volutpat nec velit odio. Pharetra purus nunc ac risus eleifend.</div>
+        <a type="button" class="btn primaryBackgroundColor colorWhite mt-3 px-4">Book an appointment</a>
+      </div>
+      <div class="col-6">
+        <div class="about-img">
+          <img src="/assets/images/about-img.png" class="img-fluid" alt="about image">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+
 
 <?php require(__DIR__ . '/views/includes/footer.php'); ?>
