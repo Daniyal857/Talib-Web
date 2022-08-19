315c17b22ed0639ffd9ef21bb68da5f34d491fe0
DaniyalKhan <daniyalkhan857@gmail.com>
2022-08-19 23:22:25 +0500

    poduct page name update - product detail and product category

---
+++ product-detail.php
@@ -0,0 +1,29 @@
+<?php
+$pageClass = "";
+require(__DIR__ . '/views/includes/header.php');
+?>
+
+<!-- NAVIGATION AND BANNER SECTION 
+  ================================================== -->
+<div class="innerpage">
+
+    <section class="heroSection position-relative">
+        <?php require(__DIR__ . '/views/includes/navigation.php'); ?>
+    </section>
+
+    <section class="section-container product-details-page position-relative py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col d-flex justify-content-between align-items-center mb-3">
+                    <h1 class="pageTitle text-center">Item Details</h1>
+                </div>
+            </div>
+
+
+
+        </div>
+    </section>
+</div>
+
+
+<?php require(__DIR__ . '/views/includes/footer.php'); ?>
